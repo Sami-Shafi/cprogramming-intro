e2aea8ad255091f3857be7d06d6a096dce99b531
Sami-Shafi <freelancersami123@gmail.com>
2025-04-06 19:59:11 +0600

    experiment with c data types and behaviour

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main ()
 {
-    printf("Hello Mother!");
+    char hey = 's';
+    int num1;
+    printf("%d \n", num1);
+    num1 = 10;
+    printf("%d \n", num1);
+    printf("%c", hey);
     return 0;
 }
