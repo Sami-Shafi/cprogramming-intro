395acdf28f594736eaa4c1da18f570c3dc727474
Sami-Shafi <freelancersami123@gmail.com>
2025-04-07 01:10:48 +0600

    taking input

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main ()
 {
-    char hey = 's';
-    int num1;
-    printf("%d \n", num1);
-    num1 = 10;
-    printf("%d \n", num1);
-    printf("%c", hey);
+    int age;
+    printf("Please enter your age: \n");
+    scanf("%d", &age);
+
+    printf("Your age is: %d", age);
+
     return 0;
 }
