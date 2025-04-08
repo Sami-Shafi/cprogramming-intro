c911d467a9bb19408015a8469a879d721c321782
Sami-Shafi <freelancersami123@gmail.com>
2025-04-08 19:10:44 +0600

    Float Division

--- main.c
+++ main.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 int main ()
 {
-    int first = 10;
+    int first = 17;
     int second = 2;
-    
-    int sum = first + second;
-    printf("Total is: %d \n", sum);
-
-    int sub = first - second;
-    printf("Subtraction Result: %d \n", sub);
-
-    int multiply = first * second;
-    printf("Multiplication Result: %d \n", multiply);
 
     int div = first / second;
-    printf("Division Result: %d", div);
 
-    
+    printf("The Result: %d", div);
 
     return 0;
 }
