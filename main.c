1f07a8eda38ca7e29d2d16dee1b1700cc2c39a51
Sami-Shafi <freelancersami123@gmail.com>
2025-04-10 17:27:33 +0600

    summation loop

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 int main ()
 {
-    int cash;
+    int sum = 0;
+    int limit;
+    int start = 1;
+    printf("Please Enter Maximum Number: \n");
+    scanf("%d", &limit);
 
-    if (cash >= 100) {
-        printf("Platter Khabo");
-    }else if (cash >= 50)
-    {
-        printf("Fuchka Khawa Jay");
-    }else {
-        printf("Fokir Amra! Kosto!");
+    for (int i = start; i<=limit; i+=1) {
+        sum += i;
+        printf("Value Updated: %d \n", sum);
     }
-    
+
+    printf("--------- \n");
+    printf("Total Summation: %d!", sum);
 
     return 0;
 }
