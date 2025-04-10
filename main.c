d82d52acb49f6dbb9a62aa8564b9f5992405b1f6
Sami-Shafi <freelancersami123@gmail.com>
2025-04-10 17:35:44 +0600

    even-odd with loop

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main ()
 {
-    int sum = 0;
     int limit;
     int start = 1;
     printf("Please Enter Maximum Number: \n");
     scanf("%d", &limit);
 
     for (int i = start; i<=limit; i+=1) {
-        sum += i;
-        printf("Value Updated: %d \n", sum);
+        printf("%d is ", i);
+        if(i%2==0){
+            printf("even! \n");
+        }else {
+            printf("odd! \n");
+        }
     }
-
-    printf("--------- \n");
-    printf("Total Summation: %d!", sum);
 
     return 0;
 }
