b71a2adc7f7d42ba2b25230ace7fbf5d0fca227a
Sami-Shafi <freelancersami123@gmail.com>
2025-04-15 05:13:25 +0600

    Multiples Problem

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    // Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
-    // Only one line containing two numbers A and B (0  ≤  A, B  ≤  100).
+    // Given two numbers A and B. Print "Multiples" if A is multiple of B or vice versa. Otherwise print "No Multiples".
+    // Only one line containing two numbers A, B (1  ≤  A, B  ≤  10^6)
     
     int a;
     int b;
     scanf("%d %d", &a, &b);
 
-    if(a>=0 && b>=0 && a<=100 && b<=100){
-        if ( a >= b) {
-            printf("Yes");
+    if(a>=1 && b>=1 && a<=1000000 && b<=1000000){
+        if ( a % b == 0 || b % a == 0) {
+            printf("Multiples");
         }else {
-            printf("No");
+            printf("No Multiples");
         }
     }
     
