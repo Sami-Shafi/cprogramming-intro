d63a7f0a8ae46324abaf7e53993b99c292ebacd3
Sami-Shafi <freelancersami123@gmail.com>
2025-04-15 04:37:46 +0600

    Basic Comparision Problem

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-int main ()
-{
-    int limit;
-    int start = 1;
-    printf("Please Enter Maximum Number: \n");
-    scanf("%d", &limit);
 
-    for (int i = start; i<=limit; i+=1) {
-        printf("%d is ", i);
-        if(i%2==0){
-            printf("even! \n");
+int main() {
+    // Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
+    // Only one line containing two numbers A and B (0  ≤  A, B  ≤  100).
+    
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+
+    if(a>=0 && b>=0 && a<=100 && b<=100){
+        if ( a >= b) {
+            printf("Yes");
         }else {
-            printf("odd! \n");
+            printf("No");
         }
     }
-
+    
     return 0;
 }
