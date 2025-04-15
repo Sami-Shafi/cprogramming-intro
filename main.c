72457c6da070d0e9102504e8c2742ed1f3f38b61
Sami-Shafi <freelancersami123@gmail.com>
2025-04-15 18:05:12 +0600

    Biggest integer among 3

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    // Given a letter X. Determine whether X is Digit or Alphabet and if it is Alphabet determine if it is Capital Case or Small Case. Print a single line contains "IS DIGIT" if X is digit otherwise, print "ALPHA" in the first line followed by a new line that contains "IS CAPITAL" if X is a capital letter and "IS SMALL" if X is a small letter.
 
-    char letter;
-    scanf("%c", &letter);
-    int letterCode = letter;
 
-    if(letterCode <= 57 && letterCode >= 48) {
-        printf("IS DIGIT");
-    }else {
-        printf("ALPHA \n");
-        if(letterCode <= 122 && letterCode >= 97){
-            printf("IS SMALL");
-        } else if(letterCode <= 90 && letterCode >= 65) {
-            printf("IS CAPITAL");
-        }    
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int lowerLimit = -100000;
+    int upperLimit = 100000;
+
+    if (a >= lowerLimit && a <= upperLimit && b >= lowerLimit && b <= upperLimit && c >= lowerLimit && c <= upperLimit) {
+        int highest = a;
+        if (b > a) {
+            highest = b;
+        }
+        if (c > highest) {
+            highest = c;
+        }
+        printf("%d", highest);
     }
     
     return 0;
