6e938263105dc5959a6f35609f826ae5ea9f9604
Sami-Shafi <freelancersami123@gmail.com>
2025-04-15 22:49:43 +0600

    Even Odd Pos Neg Counter

--- main.c
+++ main.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 
 int main() {
+    // Given N numbers. Count how many of these values are even, odd, positive and negative.
 
-    int x = 10;
-    int y = ++x;
-    int z = y++;
-    printf("%d \n", x);
-    printf("%d \n", y);
-    printf("%d \n", z);
-    printf("%d \n", z++);
-    printf("%d ", z);
+    // Input
+    // First line contains one number N (1 ≤ N ≤ 103) number of values.
 
-    printf("\n \n");
+    // Second line contains N numbers (-105 ≤ Xi ≤ 105).
 
-    int x2 = 10;
-    ++x2;
-    printf("%d", ++x2);
+    // Output
+    // Print four lines with the following format:
+    // First Line: "Even: X", where X is the number of even numbers in the given input.
+    // Second Line: "Odd: X", where X is the number of odd numbers in the given input.
+    // Third Line: "Positive: X", where X is the number of positive numbers in the given input.
+    // Fourth Line: "Negative: X", where X is the number of negative numbers in the given input.
+
+    int valueCount, tempNum, evenCount = 0, oddCount = 0, posCount = 0, negCount = 0;
+    scanf("%d", &valueCount);
+
+    for (int i = 1; i <= valueCount; i++)
+    {
+        scanf("%d", &tempNum);
+        
+        if(tempNum % 2 == 0) {
+            evenCount += 1;
+        }else {
+            oddCount += 1;
+        }
+
+        if(tempNum < 0) {
+            negCount += 1;
+        }else {
+            posCount += 1;
+        }
+    }
+
+    printf("Even: %d\n", evenCount);
+    printf("Odd: %d\n", oddCount);
+    printf("Positive: %d\n", posCount);
+    printf("Negative: %d", negCount);
     
     return 0;
 }
