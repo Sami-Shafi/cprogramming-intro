b492f89abb9497632745d13bfb49ef2e5eef3840
Sami-Shafi <freelancersami123@gmail.com>
2025-04-16 02:36:43 +0600

    Reverse Digit Printer Problem

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
-int main() {    
+int main() {
+    // Given a number N. Print the digits of that number from right to left separated by space.
 
-    int num, calc;
-    scanf("%d", &num);
+    // Input
+    // First line contains a number T (1 ≤ T ≤ 10) number of test cases.
 
-    for (int i = 1; i <= 12; i++) {
-        calc = num * i;
-        printf("%d * %d = %d\n", num, i, calc);
+    // Next T lines will contain a number N (0 ≤ N ≤ 109)
+
+    // Output
+    // For each test case print a single line contains the digits of the number separated by space.
+
+    int lines, line;
+    scanf("%d", &lines);
+
+    for (int i = 1; i <= lines; i++)
+    {
+        scanf("%d", &line);
+        while (line / 10 != 0){
+            printf("%d", line % 10);
+            printf(" ");
+            line /= 10;
+        }
+        printf("%d", line % 10);
+        printf("\n");
     }
 
     return 0;
