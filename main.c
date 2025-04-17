24791e3b1bf64df90f88410f95ee518e6773003e
Sami-Shafi <freelancersami123@gmail.com>
2025-04-18 04:52:06 +0600

    Loop Practice Phitron Assignment 2.5

--- main.c
+++ main.c
@@ -2,40 +2,57 @@
 
 int main() {
     
-    // You will be given a positive integer N and N numbers after that. You need to tell the sum of positive numbers and the sum of negative numbers separated by a space.
+    // You will given a positive integer N and an array A of size N. Also you will be given two values X and V. You need to change the value of X'th index to V and then print the array in reverse way.
+
+    // Note: Index starts from 0.
 
     // Input Format
-    
+
     // First line will contain N.
-    // Second line will contain N values named V.
+    // Second line will contain the array A.
+    // Third line will contain X and V.
     // Constraints
-    
+
     // 1 <= N <= 10^5
-    // -1000 <= V <= 1000
+    // 1 <= A[i] <= 100 ; where A[i] are the values of array A.
+    // 0 <= X < N
+    // 1 <= V <= 100
     // Output Format
-    
-    // Output the sum of positive numbers first, then sum of negative numbers.
-    
-    int N, sumP = 0, sumN = 0, temp;
-    scanf("%d", &N);
 
-    if(N >= 1 && N <= 100000) {
-        for (int i = 1; i <= N; i++)
-        {
-            scanf("%d", &temp);
-            if(temp >= -1000 && temp <= 1000) {
-                if (temp >= 0)
-                {
-                    sumP += temp;
-                }else {
-                    sumN += temp;
-                }
-            }
+    // Output the final array in reverse order.
+
+    int arraySize, valueInsideArray;
+    scanf("%d", &arraySize);
+    if (arraySize <= 0 || arraySize > 100000) {return 1;}
+
+    int ourArray[arraySize];
+
+    for (int i = 0; i < arraySize; i++)
+    {
+        scanf("%d", &valueInsideArray);
+        
+        if(valueInsideArray > 0 && valueInsideArray <= 100) {
+            ourArray[i] = valueInsideArray;
         }
-
-        printf("%d %d", sumP, sumN);
     }
-    
+
+    int targetIndex, changedVal;
+    scanf("%d", &targetIndex);
+    scanf("%d", &changedVal);
+
+    if (targetIndex >= 0 && targetIndex < arraySize && changedVal >= 1 && changedVal <= 100) {
+        ourArray[targetIndex] = changedVal;
+    }
+
+    for (int i = arraySize-1; i >= 0; i--)
+    {
+        printf("%d", ourArray[i]);
+        if (i > 0)
+        {
+            printf(" ");
+        }
+        
+    }
     
     return 0;
 }
