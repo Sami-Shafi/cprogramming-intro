d2dfe0b2cc0a68dcadeb37c6f3105757c4b8efb7
Sami-Shafi <freelancersami123@gmail.com>
2025-04-21 07:56:57 +0600

    push a value inside an array at a certain index

--- main.c
+++ main.c
@@ -2,57 +2,28 @@
 
 int main() {
     
-    // You will given a positive integer N and an array A of size N. Also you will be given two values X and V. You need to change the value of X'th index to V and then print the array in reverse way.
+    int size;
+    scanf("%d", &size);
 
-    // Note: Index starts from 0.
+    int array[size+1];
 
-    // Input Format
-
-    // First line will contain N.
-    // Second line will contain the array A.
-    // Third line will contain X and V.
-    // Constraints
-
-    // 1 <= N <= 10^5
-    // 1 <= A[i] <= 100 ; where A[i] are the values of array A.
-    // 0 <= X < N
-    // 1 <= V <= 100
-    // Output Format
-
-    // Output the final array in reverse order.
-
-    int arraySize, valueInsideArray;
-    scanf("%d", &arraySize);
-    if (arraySize <= 0 || arraySize > 100000) {return 1;}
-
-    int ourArray[arraySize];
-
-    for (int i = 0; i < arraySize; i++)
+    for (int i = 0; i < size; i++)
     {
-        scanf("%d", &valueInsideArray);
-        
-        if(valueInsideArray > 0 && valueInsideArray <= 100) {
-            ourArray[i] = valueInsideArray;
-        }
+        scanf("%d", &array[i]);
     }
 
-    int targetIndex, changedVal;
-    scanf("%d", &targetIndex);
-    scanf("%d", &changedVal);
-
-    if (targetIndex >= 0 && targetIndex < arraySize && changedVal >= 1 && changedVal <= 100) {
-        ourArray[targetIndex] = changedVal;
-    }
-
-    for (int i = arraySize-1; i >= 0; i--)
+    int index, val;
+    scanf("%d %d", &index, &val);
+    for (int i = size; i > index; i--)
     {
-        printf("%d", ourArray[i]);
-        if (i > 0)
-        {
-            printf(" ");
-        }
-        
+        array[i] = array[i-1];
     }
-    
+    array[index] = val;
+
+    for (int i = 0; i < size+1; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
     return 0;
 }
