31d9af6093b39600382859150436a0c03019e61e
Sami-Shafi <freelancersami123@gmail.com>
2025-04-25 02:59:10 +0600

    digit summation

--- main.c
+++ main.c
@@ -2,14 +2,19 @@
 #include <string.h>
 
 int main() {
-    
-    char s[1000001];
-    fgets(s, 1000001, stdin);
+    // A program that will serially take all digits from a number and sum them. Like, if you put 23434 it will return sum of 2+3+4+3+4
 
-    for (int i = 0; s[i] != '\\'; i++)
+    char s[1000001];
+    scanf("%s", s);
+
+    int sum = 0;
+    for (int i = 0; i < strlen(s); i++)
     {
-        printf("%c", s[i]);
+        // when we get it with s[i] it will return the ASCII value. To get integer we have to subtract 48 from it
+        sum += s[i]-'0';
     }
+
+    printf("%d", sum);
     
     return 0;
 }
