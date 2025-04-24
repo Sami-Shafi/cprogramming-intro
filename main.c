6f0e0e15e3eab2e212115b16d03bd002bb25cfba
Sami-Shafi <freelancersami123@gmail.com>
2025-04-24 13:47:13 +0600

    String Length Manual

--- main.c
+++ main.c
@@ -4,6 +4,14 @@ int main() {
     
     char a[30] = "SAMIULLAH IN SHAFI";
     printf("%s", a);
+    int length = 0;
+    for (int i = 0; a[i] != '\0'; i++)
+    {
+        length++;
+    }
+
+    printf("\n%d", length);
+    
 
     return 0;
 }
