6e59a8102ebc8caf36fbae448b6967cc1081aea9
Sami-Shafi <freelancersami123@gmail.com>
2025-04-26 04:18:47 +0600

    compare lexicographical size of string manually

--- main.c
+++ main.c
@@ -6,11 +6,18 @@ int main() {
     char a[101], b[101];
     scanf("%s %s", &a, &b);
 
-    // strcpy(a, b);
-    // strcat(a, b);
-    strcat(b, a);
-
-    printf("%s %s", a, b);
+    for (int i = 0; i < 101; i++) {
+        if(a[i] == "\0" && b[i] == "\0") {
+            printf("Both Strings are the same!");
+        }else if(a[i] == "\0" || a[i] < b[i]) {
+            printf("A is smaller");
+        }else if (b[i] == "\0" || b[i] < a[i]) {
+            printf("B is smaller");
+        }else if (a[i] == b[i]) {
+            continue;
+        }
+        break;
+    }
 
     return 0;
 }
