5f8b611530e708876cf80daaad9b9c2d0731ad11
Sami-Shafi <freelancersami123@gmail.com>
2025-04-26 03:07:34 +0600

    String copy, concatenate

--- main.c
+++ main.c
@@ -3,51 +3,14 @@
 
 int main() {
 
-    // Given a string S. Print the origin string if it's not too long otherwise, print the special abbreviation.
+    char a[101], b[101];
+    scanf("%s %s", &a, &b);
 
-    // Note: The string is called too long, if its length is strictly more than 10 characters. If the string is too long then you have to print the string in the following manner:
+    // strcpy(a, b);
+    // strcat(a, b);
+    strcat(b, a);
 
-    // Print the first character in the string.
-    // Print number of characters between the first and the last characters.
-    // Print the last character in the string.
-    // For example: "localization" will be "l10n", and "internationalization" will be "i18n".
-    
-    int size;
-    scanf("%d", &size);
+    printf("%s %s", a, b);
 
-    char array[size][100];
-
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%s", &array[i]);
-    }
-    
-    // loop in all words
-    for (int i = 0; i < size; i++)
-    {
-        if(strlen(array[i]) > 10) {
-            char firstDig = array[i][0];
-            int length = 0;
-            char lastDig;
-
-            // loop inside each word
-            for (int d = 0; d < strlen(array[i]); d++)
-            {
-                if (d > 0 && d < strlen(array[i])-1){
-                    length++;
-                }
-
-                if (strlen(array[i])-d == 1) {
-                    lastDig = array[i][d];
-                }
-            }
-            
-            printf("%c%d%c\n", firstDig, length, lastDig);
-        }else {
-            printf("%s\n", array[i]);
-        }
-    }
-    
-    
     return 0;
 }
