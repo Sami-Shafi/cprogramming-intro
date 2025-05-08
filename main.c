12794f0dd2eb2d3cecbd2be9b3e31ae0c693ea74
Sami-Shafi <freelancersami123@gmail.com>
2025-05-09 05:03:20 +0600

    diamond creator with input of size

--- main.c
+++ main.c
@@ -1,18 +1,50 @@
 #include <stdio.h>
 #include <math.h>
 
+int printSpaces(spacesVar) {
+    for (int i = 1; i <= spacesVar; i++)
+    {
+        printf(" ");
+    }
+}
+
+int printStars(starsVar) {
+    for (int i = 1; i <= starsVar; i++)
+    {
+        printf("*");
+    }
+}
+
 int main() {
 
-    // int ans = ceil(4.1);
-    // int ans = floor(4.9);
-    // int ans = round(4.5);
-    // int ans = round(4.3);
-    // float ans = sqrt(132);
-    // int ans = pow(2, 4);
-    int ans = abs(-10);
+    int n;
+    scanf("%d", &n);
 
-    printf("%d", ans);
-    // printf("%f", ans);
+    int step = 1, stars = 1, spaces = n-1;
+    while (step <= n)
+    {
+        printSpaces(spaces);
+        printStars(stars);
+        printf("\n");
+        
+        spaces--;
+        stars+=2;
+        step++;
+    }
+    
+    // clearing extra 2 stars after loop
+    stars-=2;
+    while (step > 0)
+    {
+        spaces++;
+        printSpaces(spaces);
+        printStars(stars);
+        printf("\n");
+        
+        stars-=2;        
+        step--;
+    }
+    
     
     return 0;
 }
