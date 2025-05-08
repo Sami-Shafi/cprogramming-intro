ae508efc6748ad2a8b1648cce1f1658d800f2b4f
Sami-Shafi <freelancersami123@gmail.com>
2025-05-09 04:18:04 +0600

    basic math functions

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <string.h>
-
-void sum() {
-    int x = 20;
-    printf("SUM: %d\n", x);
-}
+#include <math.h>
 
 int main() {
 
-    int x = 10;
-    printf("MAIN: %d\n", x);
-    sum();
+    // int ans = ceil(4.1);
+    // int ans = floor(4.9);
+    // int ans = round(4.5);
+    // int ans = round(4.3);
+    // float ans = sqrt(132);
+    // int ans = pow(2, 4);
+    int ans = abs(-10);
+
+    printf("%d", ans);
+    // printf("%f", ans);
     
     return 0;
 }
