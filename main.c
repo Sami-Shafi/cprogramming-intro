55bdac49e43f98fbd9656e4f8de5225f68047014
Sami-Shafi <freelancersami123@gmail.com>
2025-05-09 03:23:35 +0600

    function - sum

--- main.c
+++ main.c
@@ -1,34 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+int sum(int num1, int num2) {
+    int ans = num1 + num2;
+    return ans;
+}
+
 int main() {
 
-    int size, sum;
-    scanf("%d", &size);
-    int arr[size];
-    
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    
-    for (int i = 0; i < size-1; i++)
-    {
-        for (int j = i+1; j < size; j++)
-        {
-            if(arr[i] < arr[j]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    
+    int val = sum(20, 5);
+    printf("%d", val);
     
     return 0;
 }
