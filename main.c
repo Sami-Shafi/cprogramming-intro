799b842e154c4d182eac025aa1087da96c0b6e87
Sami-Shafi <freelancersami123@gmail.com>
2025-05-09 06:13:01 +0600

    pointers intro

--- main.c
+++ main.c
@@ -1,50 +1,17 @@
 #include <stdio.h>
-#include <math.h>
-
-int printSpaces(spacesVar) {
-    for (int i = 1; i <= spacesVar; i++)
-    {
-        printf(" ");
-    }
-}
-
-int printStars(starsVar) {
-    for (int i = 1; i <= starsVar; i++)
-    {
-        printf("*");
-    }
-}
 
 int main() {
-
-    int n;
-    scanf("%d", &n);
-
-    int step = 1, stars = 1, spaces = n-1;
-    while (step <= n)
-    {
-        printSpaces(spaces);
-        printStars(stars);
-        printf("\n");
-        
-        spaces--;
-        stars+=2;
-        step++;
-    }
     
-    // clearing extra 2 stars after loop
-    stars-=2;
-    while (step > 0)
-    {
-        spaces++;
-        printSpaces(spaces);
-        printStars(stars);
-        printf("\n");
-        
-        stars-=2;        
-        step--;
-    }
-    
+    int x = 10;
+    printf("%p\n", &x);
+
+    int* pointer;
+    pointer = &x;
+
+    // location of where x is stored
+    printf("%p\n", pointer);
+    // location of where pointer variable is stored
+    printf("%p", &pointer);
     
     return 0;
 }
