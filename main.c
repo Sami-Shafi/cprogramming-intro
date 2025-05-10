ee240082f04166e3051fa80b30ee6e42780ce219
Sami-Shafi <freelancersami123@gmail.com>
2025-05-10 23:15:04 +0600

    even odd count with function

--- main.c
+++ main.c
@@ -1,23 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-// You will be given an array A and the size of that array N. Take input in the main function. You need to write a function named count_before_one() which receives that array of integers and the size of that array and return type will be integer. The function counts the number of elements in that array until you find 1 and returns that count. Print that count in the main function.
+// You will be given an array A, and the size of that array N. You need to write a function named odd_even() and take input inside that function. The function won't return anything. You need to count the number of even values and number of odd values in that array and print inside the function.
 
-int count_before_one(int arr[], int size){
-    int ans;
-
-    for (int i = 0; i < size; i++)
-    {
-        if(arr[i] == 1) {
-            ans = i;
-            break;
-        }
-    }
-
-    return ans;
-}
-
-int main() {
+void odd_even(){
 
     int size;
     scanf("%d", &size);
@@ -28,7 +14,23 @@ int main() {
         scanf("%d", &arr[i]); 
     }
 
-    printf("%d", count_before_one(arr, size));
-    
+    int ec = 0, oc = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i]%2==0) {
+            ec++;
+        }else{
+            oc++;
+        }
+    }
+
+    printf("%d %d", ec, oc);
+}
+
+int main() {
+
+    odd_even();
+
     return 0;
 }
