5ab219c9400283f113cd4ca8cf476f165f7162b1
Sami-Shafi <freelancersami123@gmail.com>
2025-05-14 03:12:55 +0600

    2 Dimensional Arrays

--- main.c
+++ main.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-// You will be given an array A, and the size of that array N. You need to write a function named odd_even() and take input inside that function. The function won't return anything. You need to count the number of even values and number of odd values in that array and print inside the function.
-
-void odd_even(){
-
-    int size;
-    scanf("%d", &size);
-    int arr[size];
-
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%d", &arr[i]); 
-    }
-
-    int ec = 0, oc = 0;
-
-    for (int i = 0; i < size; i++)
-    {
-        if(arr[i]%2==0) {
-            ec++;
-        }else{
-            oc++;
-        }
-    }
-
-    printf("%d %d", ec, oc);
-}
-
 int main() {
 
-    odd_even();
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int arr[r][c];
+
+    // each row loop
+    for (int i = 0; i < r; i++)
+    {
+        // values in each column
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+        
+    }
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
 
     return 0;
 }
