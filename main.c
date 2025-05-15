9c1ae6a94d70f26819f566241b7f0563bc48cf3a
Sami-Shafi <freelancersami123@gmail.com>
2025-05-15 19:08:27 +0600

    Matrix  Reverse

--- main.c
+++ main.c
@@ -3,14 +3,24 @@
 
 int main() {
 
+    // Given two numbers N, M
+    // and a 2D array of size N * M. Print the inverted array that appeared in the mirror.
+
+    // Input
+    // First line contains two numbers N, M (1≤N,M≤100)
+    // N donates number of rows and M donates number of columns.
+
+    // Each of the next N lines will contain M numbers (1≤Ai,j≤109).
+
+    // Output
+    // Print the inverted array.
+
     int r, c;
     scanf("%d %d", &r, &c);
     int arr[r][c];
 
-    // each row loop
     for (int i = 0; i < r; i++)
     {
-        // values in each column
         for (int j = 0; j < c; j++)
         {
             scanf("%d", &arr[i][j]);
@@ -20,13 +30,12 @@ int main() {
 
     for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < c; j++)
+        for (int j = c-1; j >= 0; j--)
         {
             printf("%d ", arr[i][j]);
         }
         printf("\n");
     }
-    
     
 
     return 0;
