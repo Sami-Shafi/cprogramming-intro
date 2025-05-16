b634732e6ce7d93c7bdb83769193850a843f990e
Sami-Shafi <freelancersami123@gmail.com>
2025-05-16 06:04:26 +0600

    Array from substraction problem

--- main.c
+++ main.c
@@ -1,99 +1,84 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
+#include <math.h>
+
+// You will be given an array A and the size of that array N. You need to make a copy of this array named B. Then sort the array B in ascending order.
+
+// After that, you need to make another array C, where each index i (0 <= i < N) of array C is the absolute difference between array A[i] and B[i].
+
+// That means,
+// C[0] = |A[0] - B[0]|
+// C[1] = |A[1] - B[1]|
+// C[2] = |A[2] - B[2]|
+// .....
+// .....
+// C[N-1] = |A[N-1] - B[N-1]|
+
+// Here, || means the absolute value. After making the array C, print that array.
+
+// Input Format
+// First line will contain T, the number of test cases.
+// First line of each test case will contain N.
+// Second line of each test case will contain the array A.
 
 
-// Christmas Tree Generator
 // Sample Input 0
-// 1
+// 2
+// 5
+// 5 4 3 2 1
+// 6
+// 2 4 5 6 5 1
 
 // Sample Output 0
-//      *
-//     ***
-//    *****
-//   *******
-//  *********
-// ***********
-//      *
-//      *
-//      *
-//      *
-//      *
+// 4 2 0 2 4 
+// 1 2 1 1 0 5 
 
-// Sample Input 1
-// 3
-
-// Sample Output 1
-//       *
-//      ***
-//     *****
-//    *******
-//   *********
-//  ***********
-// *************
-//      ***
-//      ***
-//      ***
-//      ***
-//      ***
-
-// Sample Input 2
-// 5
-
-// Sample Output 2
-//        *
-//       ***
-//      *****
-//     *******
-//    *********
-//   ***********
-//  *************
-// ***************
-//      *****
-//      *****
-//      *****
-//      *****
-//      *****
-
-void makeBranch(int stars, int spaces,int max) {
-    if (stars > max) {
-        return;
-    }
-    for (int i = 1; i <= spaces; i++) {
-        printf(" ");
-    } 
-    for (int i = 1; i <= stars; i++)
-    {
-        printf("*");
-    }
-    printf("\n");
-    makeBranch(stars+2, spaces-1, max);
+void replaceArrVal(int arr[], int num1, int num2) {
+    int temp = arr[num1];
+    arr[num1] = arr[num2];
+    arr[num2] = temp;
 }
 
-void makeBody(int n) {
-    for (int i = 0; i < 5; i++)
+void printArr(int arr[], int size){
+    for (int i = 0; i < size; i++)
     {
-        for (int i = 1; i <= 5; i++) {
-        printf(" ");
-        } 
-        for (int i = 1; i <= n; i++)
-        {
-            printf("*");
-        }
-        printf("\n");
+        printf("%d ", arr[i]);
     }
-    
+    printf("\n"); 
 }
 
 int main() {
 
-    int n;
-    scanf("%d", &n);
+    int t;
+    scanf("%d", &t);
 
-    if(n % 2 != 0 && n > 0 && n < 22) {
-        int highest = n+10;
-        makeBranch(1, (highest-1)/2, highest);
-        makeBody(n);
+    for (int i = 0; i < t; i++)
+    {
+        int size;
+        scanf("%d", &size);
+        int arr1[size], arr2[size], arr3[size];
+        for (int i = 0; i < size; i++)
+        {
+            scanf("%d", &arr1[i]);
+            arr2[i] = arr1[i];
+        }
+
+        for (int i = 0; i < size-1; i++)
+        {
+            for (int j = i+1; j < size; j++)
+            {
+                if(arr2[i] > arr2[j]) {
+                    replaceArrVal(arr2, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            int val = arr1[i] - arr2[i];
+            arr3[i] = abs(val);
+        }
+
+        printArr(arr3, size);
     }
 
     return 0;
